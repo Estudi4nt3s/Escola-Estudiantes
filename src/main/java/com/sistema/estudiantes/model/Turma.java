287e2ac0dd0b13e4ca38a03285d5a70a2c23f464
DaviLacerdaHub <davioliveiralac@gmail.com>
2026-02-24 08:51:47 -0300

    Remove atributo (curso) de turmaDAO e turma model

--- src/main/java/com/sistema/estudiantes/model/Turma.java
+++ src/main/java/com/sistema/estudiantes/model/Turma.java
@@ -3,23 +3,20 @@ package com.sistema.estudiantes.model;
 public class Turma extends ModelBase {
     private int ano;
     private String serie;
-    private String curso;
     private char letra;
 
     public Turma() {}
 
-    public Turma(int ano, String serie, String curso, char letra) {
+    public Turma(int ano, String serie, char letra) {
         this.ano = ano;
         this.serie = serie;
-        this.curso = curso;
         this.letra = letra;
     }
 
-    public Turma(int id, int ano, String serie, String curso, char letra) {
+    public Turma(int id, int ano, String serie, char letra) {
         super(id);
         this.ano = ano;
         this.serie = serie;
-        this.curso = curso;
         this.letra = letra;
     }
 
@@ -34,9 +31,6 @@ public class Turma extends ModelBase {
 
     public String getSerie(){ return this.serie;}
 
-    public String getCurso() {
-        return this.curso;
-    }
 
     public char getLetra() {
         return this.letra;
@@ -46,15 +40,12 @@ public class Turma extends ModelBase {
         this.ano = ano;
     }
 
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
 
     public void setLetra(char letra) {
         this.letra = letra;
     }
 
     public String toString() {
-        return "Id: " + getId() + "\nAno: " + this.ano + "\nSérie:" + serie + "\nCurso: " + this.curso + "\nLetra: " + this.letra;
+        return "Id: " + getId() + "\nAno: " + this.ano + "\nSérie:" + serie + "\nLetra: " + this.letra;
     }
 }
