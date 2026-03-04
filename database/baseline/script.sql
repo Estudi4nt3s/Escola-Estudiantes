6dabe5c9a088fecd0d350ed29bc44bc4707b836b
eduardo policarpo <epolica1@jbssa.com>
2026-03-04 05:43:41 -0700

    aaaaa

--- database/baseline/script.sql
+++ database/baseline/script.sql
@@ -2,25 +2,28 @@ drop table if exists Nota;
 drop table if exists Observacao;
 drop table if exists ProfessorDisciplina;
 drop table if exists TurmaAluno;
-drop table if exists Professor;
 drop table if exists Aluno;
 drop table if exists Aula;
 drop table if exists Turma;
 drop table if exists Disciplina;
+drop table if exists Professor;
 drop table if exists Usuario;
+drop table if exists Admin;
 
 create table Usuario ( Id               serial          not null
+                     , Nome             varchar(255)    not null
+                     , Sobrenome        varchar(255)    not null
                      , Email            varchar(255)    not null
                      , Senha            varchar(255)    not null
-                     , isAdm            boolean         not null default false
                      , Photo            text                null
                      , constraint pk_Usuario        primary key (id)
                      , constraint uq_Usuario_Email  unique (Email)
                      );
 
-create table Professor ( Id     	serial          not null
-                       , Nome   	varchar(60)     not null
-                       , UsuarioId	int					null
+create table Professor ( Id     	    serial          not null
+                       , Nome   	    varchar(60)     not null
+                       , UsuarioId	    int					null
+                       , DisciplinaId   int             not null
                        , constraint pk_Professor primary key (Id)
                        );
 
@@ -29,20 +32,12 @@ create table Disciplina ( Id    serial      not null
                         , constraint pk_Disciplina primary key (Id)
                         );
 
-create table ProfessorDisciplina ( Id           serial  not null
-                                 , ProfessorId  int     not null
-                                 , DisciplinaId int     not null
-                                 , constraint pk_ProfessorDisciplina            primary key (Id)
-                                 , constraint fk_ProfessorDisciplina_Professor  foreign key (ProfessorId)   references Professor(Id)
-                                 , constraint fk_ProfessorDisciplina_Disciplina foreign key (DisciplinaId)  references Disciplina(Id)
-                                 );
-
 create table Aluno ( Matricula      serial          not null
 			       , Cpf			varchar(11)     not null
-                   , Nome           varchar(200)    not null
                    , DataNascimento date                null
 				   , UsuarioId  	int                 null
                    , TelefonePai    varchar(15)     not null
+                   , TurmaId        int                 null
                    , constraint pk_aluno primary key (Matricula)
                    );
 
@@ -53,22 +48,12 @@ create table Turma ( Id     	serial      not null
                    , constraint pk_Turma primary key (Id)
                    );
 
-create table TurmaAluno ( Id                serial  not null
-                        , MatriculaAluno    int     not null
-                        , IdTurma           int     not null
-                        , constraint pk_TurmaAluno          primary key (Id)
-                        , constraint fk_TurmaAluno_Aluno    foreign key (MatriculaAluno)   references Aluno(Matricula)
-                        , constraint fk_TurmaAluno_Turma    foreign key (IdTurma)          references Turma(Id)
-                        );
-
 create table Observacao ( Id            serial          not null
                         , Texto         varchar(2000)   not null
                         , DataCriacao   date            not null default current_date
-                        , ProfessorId   int             not null
                         , IdAluno       int             not null
                         , DisciplinaId  int             not null
-                        , constraint pk_Observacao primary key (Id)
-                        , constraint fk_Observacao_Professor    foreign key (ProfessorId)   references Professor(Id)
+                        , constraint pk_Observacao              primary key (Id)
                         , constraint fk_Observacao_Aluno        foreign key (IdAluno)       references Aluno(Matricula)
                         , constraint fk_Observacao_Disciplina   foreign key (DisciplinaId)  references Disciplina(Id)
                         );
@@ -76,20 +61,27 @@ create table Observacao ( Id            serial          not null
 create table Nota ( Id              serial  not null
                   , DisciplinaId    int     not null
                   , IdAluno         int     not null
-                  , IdTurma         int     not null
-                  , Valor           numeric not null
+                  , N1              numeric not null
+                  , N2              numeric not null
                   , constraint pk_Nota              primary key (Id)
                   , constraint fk_Nota_Disciplina   foreign key (DisciplinaId)  references Disciplina(Id)
                   , constraint fk_Nota_Aluno        foreign key (IdAluno)       references Aluno(Matricula)
-                  , constraint fk_Nota_Turma        foreign key (IdTurma)       references Turma(Id)
                   );
 				  
 create table Aula	( Id            serial 	    not null
                     , HorarioInicio time        not null
                     , HorarioFim    time        not null
-                    , DisciplinaId	int	        not null
+                    , ProfessorId	int	        not null
                     , TurmaId       int         not null
                     , diaSemana     char(3)     not null
-                    , constraint fk_Aula_Disciplina  foreign key (DisciplinaId)   references Disciplina(Id)
-                    , constraint fk_Aula_Turma  foreign key (TurmaId)   references Turma(Id)
+                    , constraint pk_Aula            primary key (Id)
+                    , constraint fk_Aula_Professor  foreign key (ProfessorId)   references Professor(Id)
+                    , constraint fk_Aula_Turma      foreign key (TurmaId)       references Turma(Id)
 					);
+
+create table Admin  ( Id    serial 	        not null
+                    , Usuario varchar(255)  not null
+                    , Senha varchar(255)    not null
+                    , constraint pk_Admin           primary key (Id)
+                    , constraint uq_Admin_Usuario   unique (Usuario)
+                    );
