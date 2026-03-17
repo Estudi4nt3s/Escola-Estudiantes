ef00a80a9aa3ff88ed085c264484ca8a27b4bd77
Gustavo Sousa De Oliveira <gustavosousa-ieg@instituto.ad>
2026-03-16 23:03:01 -0300

    subindo conn

--- src/main/java/com/sistema/estudiantes/conexao/Conexao.java
+++ src/main/java/com/sistema/estudiantes/conexao/Conexao.java
@@ -1,27 +1,27 @@
 package com.sistema.estudiantes.conexao;
 
-
-//Importação Dotenv
 import io.github.cdimascio.dotenv.Dotenv;
-
-//Importações SQL
 import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Conexao {
 
     public static Connection conectar() {
         Connection conn = null;
         try {
-            Dotenv dotenv = Dotenv.load();
-            String caminho = dotenv.get("DB_URL");
-            String usuario = dotenv.get("DB_USUARIO");
-            String password = dotenv.get("DB_SENHA");
+            String caminho = System.getenv("DB_URL");
+            String usuario = System.getenv("DB_USUARIO");
+            String password = System.getenv("DB_SENHA");
 
-            // Log para conferir se os dados estão chegando (Cuidado em produção!)
+            if (caminho == null || caminho.isEmpty()) {
+                Dotenv dotenv = Dotenv.load();
+                caminho = dotenv.get("DB_URL");
+                usuario = dotenv.get("DB_USUARIO");
+                password = dotenv.get("DB_SENHA");
+            }
 
             Class.forName("org.postgresql.Driver");
+
             conn = DriverManager.getConnection(caminho, usuario, password);
 
         } catch (Exception e) {
@@ -31,17 +31,12 @@ public class Conexao {
         return conn;
     }
 
-    //Criando um método para fechar a conexão
     public static void desconectar(Connection conn){
-
         try{
-            //Verifica se a conexão esta aberta, se sim, a conexão é fechada
             if (conn != null && !conn.isClosed()){
                 conn.close();
             }
         }catch (Exception e){
-
-            //Printando erros
             e.printStackTrace();
         }
     }
